7409732cb6c3e59cec074b851245283915ad7e2d
Barak Ugav <barakugav@gmail.com>
2016-01-02 15:14:33 +0200

    [+] local storage

--- js/main.js
+++ js/main.js
@@ -1,29 +1,95 @@
-// Default '#quick-reports' tab
-if (document.location.hash == "" || document.location.hash == "#") {
-    document.location.hash = "#quick-reports";
+window.onload = function () {
+	// Default '#quick-reports' tab
+	if (document.location.hash == "" || document.location.hash == "#") {
+	    document.location.hash = "#quick-reports";
+	}
+
+	// Load 
+	var quickReports = document.getElementById("quick-reports");
+	var urlsFrom = quickReports.getElementsByClassName("urls-from")[0];
+	loadUrlsFrom("quick-reports", urlsFrom);
+};
+
+
+function loadUrlsFrom(tag, form) {
+	var sites = readLocalStorage(tag);
+	if (sites === undefined) {
+		return;
+	} else if (!(sites.length > 0)) {
+		return;
+	}
+
+	var subForms = form.getElementsByClassName("url-form");
+	for (var i = 0; i < subForms.length && i < sites.length; i++) {
+		var subForm = subForms[i];
+		var site = sites[i];
+
+		var nameBox = subForm.getElementsByClassName("name")[0];
+		var urlBox = subForm.getElementsByClassName("url")[0];
+
+		nameBox.setAttribute("value", site.name);
+		urlBox.setAttribute("value", site.url);
+	}
 }
 
-(function () {
-	var quickReports = document.getElementById("quick-reports");
-	var urlsForm = quickReports.getElementsByClassName("urls-from")[0];
-	urlsForm.style.backgroundColor = "red";
-})();
-
-function validateUrlsForm(form) {
-	var allRows = form.getElementsByClassName("url-form");
-
+function submitUrlsFrom(tag, form) {
+	// Validate
+	var subForms = form.getElementsByClassName("url-form");
 	var allValid = true;
-	for (var rowIndex in allRows) {
-		var row = allRows[rowIndex];
-		var nameBox = row.getElementsByClassName("name")[0];
-		var urlBox = row.getElementsByClassName("url")[0];
+	var sites = [];
+	for (var i = 0; i < subForms.length; i++) {
+		var subForm = subForms[i];
+		var nameBox = subForm.getElementsByClassName("name")[0];
+		var urlBox = subForm.getElementsByClassName("url")[0];
 		var name = nameBox.value;
 		var url = urlBox.value;
 
 		resetInputBorder(nameBox);
 		resetInputBorder(urlBox);
 		if (name === "" && url === "") {
-			
+			continue;
+		}
+
+		if (isValidName(name) !== true) {
+			nameBox.style.border = "solid 1px red";
+			allValid = false;
+		}
+
+		if (isValidUrl(url) !== true) {
+			urlBox.style.border = "solid 1px red";
+			allValid = false;
+		}
+
+		// Add site to sites array
+		var site = {};
+		site.name = name;
+		site.url = url;
+		sites.push(site);
+	}
+
+	if (allValid !== true) {
+		return false;
+	} else {
+		// valid
+		writeLocalStorage(tag, sites);
+	}
+
+}
+
+function validateUrlsForm(form) {
+	var subForms = form.getElementsByClassName("url-form");
+
+	var allValid = true;
+	for (var subFormIndex in subForms) {
+		var subForm = subForms[subFormIndex];
+		var nameBox = subForm.getElementsByClassName("name")[0];
+		var urlBox = subForm.getElementsByClassName("url")[0];
+		var name = nameBox.value;
+		var url = urlBox.value;
+
+		resetInputBorder(nameBox);
+		resetInputBorder(urlBox);
+		if (name === "" && url === "") {
 			continue;
 		}
 
@@ -50,6 +116,52 @@ function isValidUrl(url) {
 }
 
 function resetInputBorder(inputElement) {
-	var style = inputElement.style;
-	style.border = "2px inset";
+	inputElement.style.border = "2px inset";
+}
+
+function writeLocalStorage(tag, sites) {
+	clearLocalStorage(tag);
+
+	localStorage.setItem(tag + " count", sites.length);
+	for (var i = 0; i < sites.length; i++) {
+		var site = sites[i];
+		var name = site.name;
+		var url = site.url;
+
+		localStorage.setItem(tag + " " + i + " name", name);
+		localStorage.setItem(tag + " " + i + " url", url);
+	}
+}
+
+function readLocalStorage(tag) {
+	var countStr = localStorage.getItem(tag + " count");
+	if (countStr === undefined) {
+		return undefined;
+	}
+	var count = parseInt(countStr);
+
+	var sites = [];
+	for (var i = 0; i < count; i++) {
+		var name = localStorage.getItem(tag + " " + i + " name");
+		var url = localStorage.getItem(tag + " " + i + " url");
+
+		var site = {};
+		site.name = name;
+		site.url = url;
+		sites.push(site);
+	}
+	return sites;
+}
+
+function clearLocalStorage(tag) {
+	var countStr = localStorage.getItem(tag + " count");
+	if (countStr === null) {
+		return;
+	}
+	var count = parseInt(countStr);
+	for (var i = 0; i < count; i++) {
+		localStorage.removeItem(tag + " " + i + " name");
+		localStorage.removeItem(tag + " " + i + " url");
+	}
+	localStorage.removeItem(tag + " count");
 }
